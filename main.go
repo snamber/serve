209875bec5ae563796f98d5b9599be367e7a5de5
Stefan Amberger <stefan.amberger@catalysts.cc>
2017-06-07 18:55:59 +0200

    make path and port customizable

--- main.go
+++ main.go
@@ -4,17 +4,30 @@ import (
 	"flag"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 var dir string
+var path string
+var port string
 
 func main() {
-
+	flag.StringVar(&port, "port", "8080", "port on which to serve")
+	flag.StringVar(&path, "path", "/", "path on which files will be served")
 	flag.StringVar(&dir, "dir", ".", "directory to be served")
 	flag.Parse()
 
-	fs := http.FileServer(http.Dir(dir))
+	r := mux.NewRouter()
 
-	log.Println("serving", http.Dir(dir), "on localhost:8080")
-	http.ListenAndServe(":8080", fs)
+	// fileserver
+	fs := http.FileServer(http.Dir(dir))
+	if path != "/" {
+		fs = http.StripPrefix(path, fs)
+	}
+
+	r.PathPrefix(path).Handler(fs)
+
+	log.Println("serving", http.Dir(dir), "on localhost:"+port+path)
+	http.ListenAndServe(":"+port, r)
 }
