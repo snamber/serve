da4d5fc5937d8eee1f627cac4bf280bdba8e3c65
Stefan Amberger <stefan.amberger@catalysts.cc>
2018-12-18 16:49:51 +0100

    simplify

--- main.go
+++ main.go
@@ -2,51 +2,34 @@ package main
 
 import (
 	"flag"
-	"log"
 	"net/http"
-
-	"github.com/gorilla/mux"
-	"github.com/snamber/serve/middleware"
+	"strings"
 )
 
 var (
-	dir           string
-	path          string
-	port          string
-	basicAuthFlag bool
+	dir  string
+	path string
+	port string
 )
 
 func main() {
 	flag.StringVar(&port, "port", "8080", "port on which to serve")
 	flag.StringVar(&path, "path", "/", "path on which files will be served")
 	flag.StringVar(&dir, "dir", ".", "directory to be served")
-	flag.BoolVar(&basicAuthFlag, "basicauth", false, "turn on basic auth")
 	flag.Parse()
 
-	r := mux.NewRouter()
+	path = addSlashesToPath(path)
 
-	fs := http.FileServer(http.Dir(dir))
-	if path != "/" {
-		fs = http.StripPrefix(path, fs)
+	http.Handle(path, http.StripPrefix(path, http.FileServer(http.Dir(dir))))
+	http.ListenAndServe(":"+port, nil)
+}
+
+func addSlashesToPath(path string) string {
+	path = strings.Trim(path, "/")
+	if path == "" {
+		path = "/"
+	} else {
+		path = "/" + path + "/"
 	}
-
-	switch basicAuthFlag {
-	case true:
-		r.PathPrefix(path).Handler(
-			mw.Chain(fs.ServeHTTP,
-				mw.BasicAuth(),
-				mw.Logging(),
-			),
-		)
-	default:
-		r.PathPrefix(path).Handler(
-			mw.Chain(fs.ServeHTTP,
-				mw.Logging(),
-			),
-		)
-	}
-
-	log.Println("serving", http.Dir(dir), "on localhost:"+port+path)
-
-	http.ListenAndServe(":"+port, r)
+	return path
 }
